edaeee3385353b53656a67ec002118f6391f1285
Martin Pöpel <martin.poepel@5minds.de>
2026-02-15 01:15:15 +0100

    Update melody

--- src/core/Melody.h
+++ src/core/Melody.h
@@ -4,23 +4,26 @@
 #include "pitches.h"
 
 int melody[] = {
-        NOTE_E5, NOTE_D5, NOTE_E5, NOTE_A4,
-        0,
-        NOTE_F5, NOTE_E5, NOTE_F5, NOTE_E5, NOTE_D5,
-        0,
-        NOTE_F5, NOTE_E5, NOTE_F5, NOTE_A4,
-        0,
-        NOTE_D5, NOTE_C5, NOTE_D5, NOTE_C5, NOTE_B4, NOTE_D5, NOTE_C5
-      };
+    NOTE_E5, NOTE_E5, NOTE_E5,
+    NOTE_E5, NOTE_E5, NOTE_E5,
+    NOTE_E5, NOTE_G5, NOTE_C5, NOTE_D5,
+    NOTE_E5,
+    NOTE_F5, NOTE_F5, NOTE_F5, NOTE_F5,
+    NOTE_F5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5,
+    NOTE_E5, NOTE_D5, NOTE_D5, NOTE_E5,
+    NOTE_D5, NOTE_G5
+  };
 
 int noteDurations[] = {
-        8, 8, 2, 2,
-        4,
-        8, 8, 4, 4, 2,
-        4,
-        8, 8, 2, 2,
-        4,
-        8, 8, 4, 4, 4, 4, 2
-      };
+    8, 8, 4,
+    8, 8, 4,
+    8, 8, 8, 8,
+    2,
+    8, 8, 8, 8,
+    8, 8, 8, 16, 16,
+    8, 8, 8, 8,
+    4, 4
+  };
+
 
 #endif //TIMER_MELODY_H
